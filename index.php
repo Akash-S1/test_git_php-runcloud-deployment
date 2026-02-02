392d543c7522272cbdfee9c9ed588ed10b32e27d
AKASH S <120690517+Akash-S1@users.noreply.github.com>
2026-02-02 12:43:08 +0530

    Create index.php
    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// Simple PHP test script
+
+echo "<h1>PHP is working!</h1>";
+echo "<p>Current PHP version: " . phpversion() . "</p>";
+
+// Optional: display server info
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+?>
