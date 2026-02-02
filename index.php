750893e369f87301a87a94d62cdb92e97e1c0f5d
AKASH S <120690517+Akash-S1@users.noreply.github.com>
2026-02-02 12:56:12 +0530

    Update index.php
    commit 2

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php
 // Simple PHP test script
 
-echo "<h1>PHP is working!</h1>";
+echo "<h1>Test PHP page is working!</h1>";
 echo "<p>Current PHP version: " . phpversion() . "</p>";
 
-// Optional: display server info
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-?>
